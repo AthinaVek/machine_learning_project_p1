b7a594b96657570c757cf7920b7e49226b2f4d23
Savvas-Pol <sdi1200150@di.uoa.gr>
2020-10-13 20:18:11 +0300

    File separation

---
+++ calculations.h
@@ -0,0 +1,28 @@
+#ifndef __CALCULATIONS_H__
+#define __CALCULATIONS_H__
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <bits/stdc++.h>
+#include <string>
+#include <list> 
+#include <iterator> 
+#include <random>
+#include <vector>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+vector<int> get_s(double w, int d);
+vector<int> calculate_a(vector<unsigned char> pVec, vector<int> sVec, double w, int d);
+int calculate_h(vector<int> aVec, int m, int M, int d);
+int calculate_g(vector<int> hVec);
+
+
+
+
+
+
+#endif
--- help_functions.h
+++ help_functions.h
@@ -17,10 +17,6 @@ using namespace std;
 
 int reverseInt (int i);
 
-vector<int> get_s(double w, int d);
-vector<int> calculate_a(vector<unsigned char> pVec, vector<int> sVec, double w, int d);
-int calculate_h(vector<int> aVec, int m, int M, int d);
-int calculate_g(vector<int> hVec);
 
 // void printNestedList(list<list<unsigned char> > nested_list);
 
