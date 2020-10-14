730d69e8912363e35c9dc9965d1798b4eab832e9
AthinaVek <athina.vekraki@gmail.com>
2020-10-14 18:35:12 +0300

    Remove unused code

--- lsh.cpp
+++ lsh.cpp
@@ -1,7 +1,7 @@
 #include "help_functions.h"
 #include "calculations.h"
 
-#define m 536870912
+#define m 536870912    // 2^29
 
 using namespace std;
 
@@ -10,38 +10,41 @@ int main(int argc, char** argv){
 	string iFile, qFile, oFile;
 	int i, k, L, N;
 	double R, w;
+	int magic_number=0, number_of_images=0;
+    int n_rows=0, n_cols=0;
+    int d, M, h;
+    unsigned int g;
+
+	vector< vector<unsigned char> > pVec; 
+	vector<unsigned char> tempVec;
+	vector< vector<int> > sVec;
+	vector<int> aVec;
+	vector< vector<int> > hVec;
+	vector<int> tempIntVec;
+	vector<unsigned int> gVec;
 	
-	if(argc == 15)
-	{
-		for (int i = 1; i < 15; ++i)
-		{
-			if (argv[i] == "-d")
-			{
+	if(argc == 15){                           // Read input
+		for (int i = 1; i < 15; ++i){
+			if (argv[i] == "-d"){
 				iFile = argv[i+1];
 			}
-			else if (argv[i] == "-q")
-			{
+			else if (argv[i] == "-q"){
 				qFile = argv[i+1];
 			}
-			else if (argv[i] == "-k")
-			{
+			else if (argv[i] == "-k"){
 				k = atoi(argv[i+1]);
 			}
-			else if (argv[i] == "-L")
-			{
+			else if (argv[i] == "-L"){
 				L = atoi(argv[i+1]);
 			}
-			else if (argv[i] == "-o")
-			{
+			else if (argv[i] == "-o"){
 				oFile = argv[i+1];
 			}
-			else if (argv[i] == "-N")
-			{
+			else if (argv[i] == "-N"){
 				N = atoi(argv[i+1]);
 			}
-			else if (argv[i] == "-R")
-			{
-				R = atoi(argv[i+1]);
+			else if (argv[i] == "-R"){
+				R = atof(argv[i+1]);
 			}
 		}
 	}
@@ -58,30 +61,12 @@ int main(int argc, char** argv){
 		L = 5;
 		N = 1;
 		R = 1.0;
-		
 		w = 10 * R;
-		
 	}
 	
 	ifstream file (iFile);
-    if (file.is_open())
-    {
-        int magic_number=0, number_of_images=0;
-        int n_rows=0, n_cols=0;
-        int d, M;
-        int h;
-        
-        unsigned int g;
-        
+    if (file.is_open()){
         M = pow(2,(32/k));
-		
-		vector< vector<unsigned char> > pVec; 
-    	vector<unsigned char> tempVec;
-		vector< vector<int> > sVec;
-		vector<int> aVec;
-		vector< vector<int> > hVec;
-		vector<int> tempIntVec;
-		vector<unsigned int> gVec;
 		
         file.read((char*)&magic_number,sizeof(magic_number)); 
         magic_number = reverseInt(magic_number);
@@ -94,15 +79,9 @@ int main(int argc, char** argv){
         
         d = n_rows * n_cols;						//dimension
 
-        //list< list<unsigned char> > pList; 
-    	//list<unsigned char> tempList; 
-		
-        for(int i = 0; i < number_of_images; i++)
-        {
-            for(int r = 0; r < n_rows; r++)
-            {
-                for(int c = 0; c < n_cols; c++)
-                {
+        for(int i = 0; i < number_of_images; i++){
+            for(int r = 0; r < n_rows; r++){
+                for(int c = 0; c < n_cols; c++){
                     unsigned char temp = 0;
                     file.read((char*)&temp,sizeof(temp));
 
@@ -111,27 +90,7 @@ int main(int argc, char** argv){
             }
             pVec.push_back(tempVec);
             tempVec.erase(tempVec.begin(), tempVec.end());
-            
         }
-
-
-        //~ for (int i = 0; i < k; i++){
-        	//~ sVec = get_s(w, d);							//s_i uniform random generator
-
-        	//~ for (int j = 0; j < number_of_images; j++){
-				//~ aVec = calculate_a(pVec[j], sVec, w, d);
-				//~ h = calculate_h(aVec, m, M, d);
-				//~ tempHVec.push_back(h);
-        		//~ //cout << h << endl;
-        	//~ }
-        	//~ hVec.push_back(tempHVec);
-        	//~ tempHVec.erase(tempHVec.begin(), tempHVec.end());
-        	
-        	//~ aVec = calculate_a(pVec[0], sVec, w, d);
-        	//~ h = calculate_h(aVec, m, M, d);
-        	//~ hVec.push_back(h);
-        //~ }
-
 
         for (int i=0; i<k; i++){
 			tempIntVec = get_s(w, d);        //s_i uniform random generator
